46e170a1abd376b568fce8030672bba97a553b22
Kyle Harding <kylemharding@gmail.com>
2018-09-25 15:51:11 -0400

    allow github user to be set to populate authorized_keys

--- start.sh
+++ start.sh
@@ -3,15 +3,21 @@
 # generate ssh key if one does not exist
 if [ ! -f "/root/.ssh/id_rsa" ]
 then
-	ssh-keygen -N '' -f "/root/.ssh/id_rsa"
+	ssh-keygen -q -N '' -f "/root/.ssh/id_rsa"
+fi
+
+# generate ssh host keys
+/usr/bin/ssh-keygen -A
+
+# if a github username was provided and authorized_keys does not exist
+if [ -n "${GITHUB_USER}" ] && [ ! -e "/root/.ssh/authorized_keys" ]
+then
+	curl "https://github.com/${GITHUB_USER}.keys" > "/root/.ssh/authorized_keys"
 fi
 
 # set permissions on ssh dir
 chown -R root:root "/root/.ssh"
 chmod -R 700 "/root/.ssh"
-
-# generate ssh host keys
-/usr/bin/ssh-keygen -A
 
 # start sshd service
 if [ "${INITSYSTEM}" != "on" ]
